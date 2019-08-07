156c951e5bca4fd03d7d3e0cb78c6bac36839755
Joe Prochnow <joe.prochnow@neovera.com>
2019-08-07 17:33:29 -0400

    Add github homepage banner

--- design-practice/src/app/github/github.module.ts
+++ design-practice/src/app/github/github.module.ts
@@ -5,9 +5,11 @@ import { GithubRoutingModule } from './github-routing.module';
 import { GithubComponent } from './github.component';
 import { GithubNavbarComponent } from './github-navbar/github-navbar.component';
 import { GithubHomepageComponent } from './github-homepage/github-homepage.component';
+import { GithubBannerComponent } from './github-banner/github-banner.component';
+import { GithubButtonComponent } from './github-button/github-button.component';
 
 @NgModule({
-  declarations: [GithubComponent, GithubNavbarComponent, GithubHomepageComponent],
+  declarations: [GithubComponent, GithubNavbarComponent, GithubHomepageComponent, GithubBannerComponent, GithubButtonComponent],
   imports: [
     CommonModule,
     GithubRoutingModule
