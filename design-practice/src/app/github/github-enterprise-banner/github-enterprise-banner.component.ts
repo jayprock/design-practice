14ed84b71692fae677992919d307f8c23717e5c8
Joe Prochnow <joe.prochnow@neovera.com>
2019-08-14 16:32:03 -0400

    Add github enterprise banner

---
+++ design-practice/src/app/github/github-enterprise-banner/github-enterprise-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-enterprise-banner',
+  templateUrl: './github-enterprise-banner.component.html',
+  styleUrls: ['./github-enterprise-banner.component.css']
+})
+export class GithubEnterpriseBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- design-practice/src/app/github/github.module.ts
+++ design-practice/src/app/github/github.module.ts
@@ -7,9 +7,10 @@ import { GithubNavbarComponent } from './github-navbar/github-navbar.component';
 import { GithubHomepageComponent } from './github-homepage/github-homepage.component';
 import { GithubBannerComponent } from './github-banner/github-banner.component';
 import { GithubButtonComponent } from './github-button/github-button.component';
+import { GithubEnterpriseBannerComponent } from './github-enterprise-banner/github-enterprise-banner.component';
 
 @NgModule({
-  declarations: [GithubComponent, GithubNavbarComponent, GithubHomepageComponent, GithubBannerComponent, GithubButtonComponent],
+  declarations: [GithubComponent, GithubNavbarComponent, GithubHomepageComponent, GithubBannerComponent, GithubButtonComponent, GithubEnterpriseBannerComponent],
   imports: [
     CommonModule,
     GithubRoutingModule
