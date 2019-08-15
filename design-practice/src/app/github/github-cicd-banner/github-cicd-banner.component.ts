d3f3e86e6ce131f7fed4f73a95a11ba317cd68ad
Joe Prochnow <joe.prochnow@neovera.com>
2019-08-15 10:15:40 -0400

    Create github CI/CD banner (wip)

---
+++ design-practice/src/app/github/github-cicd-banner/github-cicd-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-cicd-banner',
+  templateUrl: './github-cicd-banner.component.html',
+  styleUrls: ['./github-cicd-banner.component.css']
+})
+export class GithubCicdBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- design-practice/src/app/github/github.module.ts
+++ design-practice/src/app/github/github.module.ts
@@ -8,9 +8,10 @@ import { GithubHomepageComponent } from './github-homepage/github-homepage.compo
 import { GithubBannerComponent } from './github-banner/github-banner.component';
 import { GithubButtonComponent } from './github-button/github-button.component';
 import { GithubEnterpriseBannerComponent } from './github-enterprise-banner/github-enterprise-banner.component';
+import { GithubCicdBannerComponent } from './github-cicd-banner/github-cicd-banner.component';
 
 @NgModule({
-  declarations: [GithubComponent, GithubNavbarComponent, GithubHomepageComponent, GithubBannerComponent, GithubButtonComponent, GithubEnterpriseBannerComponent],
+  declarations: [GithubComponent, GithubNavbarComponent, GithubHomepageComponent, GithubBannerComponent, GithubButtonComponent, GithubEnterpriseBannerComponent, GithubCicdBannerComponent],
   imports: [
     CommonModule,
     GithubRoutingModule
