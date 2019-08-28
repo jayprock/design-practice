579f4ca6f6c4655bc0846162072aca73af0891c7
Joe Prochnow <joe.prochnow@neovera.com>
2019-08-28 17:18:39 -0400

    Add drag and drop demos

--- design-practice/src/app/drag-and-drop/drag-and-drop.component.ts
+++ design-practice/src/app/drag-and-drop/drag-and-drop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-drag-and-drop',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DragAndDropComponent implements OnInit {
 
+  todos = [
+    'Todo #1',
+    'Todo #2',
+    'Todo #3',
+    'Todo #4',
+    'Todo #5'
+  ]
+
+  todos2 = [
+    'Todo #2',
+    'Todo #4',
+    'Todo #5'
+  ]
+  
+  done = [
+    'Todo #1',
+    'Todo #3'
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+  }
+
+  drop2(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, 
+                        event.container.data, 
+                        event.previousIndex, 
+                        event.currentIndex);
+    }
   }
 
 }
